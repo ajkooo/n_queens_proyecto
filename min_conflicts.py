2d84465a6d06ccf7d4d9c78f837730bf945eb0bf
RayanChek <rayanchek2007@gmail.com>
2025-11-15 17:48:04 +0100

    Update min_conflicts.py

--- min_conflicts.py
+++ min_conflicts.py
@@ -1,1 +1,13 @@
-print("hola")
+import numpy as np
+n = 8
+Q = np.random.randint(0, n, size=n)
+print(Q)
+
+def to_matrix(Q):
+    n = len(Q)
+    board = np.zeros((n, n), dtype=int)
+    for col, fila in enumerate(Q):
+        board[fila, col] = 1
+    return board
+
+print(to_matrix(Q))
