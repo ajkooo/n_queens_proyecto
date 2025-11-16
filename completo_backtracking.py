0761cd50ba33c8156f8d4121871835d5ebe5b3c4
ajkooo <ajkoupm@gmail.com>
2025-11-16 17:51:36 +0100

    Update completo_backtracking.py

--- completo_backtracking.py
+++ completo_backtracking.py
@@ -3,6 +3,12 @@ def is_valid(row, col, queens):
         if col == queens[r]: return False
         elif abs(col - queens[r]) == abs(row - r): return False
     return True
+"""
+is_valid se asegura si es posible colocar una reina en (row, col), para que no choque con las reinas ya puestas
+o no se coman entre ellas. Por cada fila (0, n), el bucle comprueba si el valor de la columna coincide con el
+valor de la columna en la que está situada una reina. No hace falta que comprobemos las filas porque ya estamos comprobando
+una fila diferente cada vez. 
+"""
     
 
 def place_queen(row, queens, n):
