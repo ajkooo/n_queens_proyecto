3067024714e608ebffe46a998cee9f31ddb60b44
ajkooo <ajkoupm@gmail.com>
2025-11-16 13:25:43 +0100

    Update ppal.py

--- ppal.py
+++ ppal.py
@@ -1,15 +1,20 @@
 """
 Programa principal
 """
-import backtracking
+import backtracking as
 import bits
 import min_conflicts
+import matplotlib.pyplot as plt
 
 N = int(input("Bienvienido al juego de las N-Reinas; introduce, a contiuacón, el tamaño del tablero"))
 
-if N < 3:
-    print("La dimensión del tablero ha de ser por lo menos 
+if N == 1:
+    print("La única solución posible es:")
+
+if N == 2 or N == 3:
+    print("La dimensión del tablero ha de ser distinta de 2 o 3")
 
 if N <= 20:
+    bactracking
     
 
