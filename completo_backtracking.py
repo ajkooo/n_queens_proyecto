216071a557c852ff9d2646e4b27ebfcf9cb49a90
ajkooo <ajkoupm@gmail.com>
2025-11-16 18:06:45 +0100

    Update completo_backtracking.py

--- completo_backtracking.py
+++ completo_backtracking.py
@@ -6,8 +6,9 @@ def is_valid(row, col, queens):
 """
 is_valid se asegura si es posible colocar una reina en (row, col), para que no choque con las reinas ya puestas
 o no se coman entre ellas. Por cada fila (0, n), el bucle comprueba si el valor de la columna coincide con el
-valor de la columna en la que está situada una reina. No hace falta que comprobemos las filas porque ya estamos comprobando
-una fila diferente cada vez. 
+valor de la columna en la que está situada una reina. No hace falta que comprobemos las filas porque el algoritmo solo
+coloca una reina por fila a la vez. También deberemos comprobar las diagonales (con reinas dispuestas a 45º unas de otras), en cuyo caso,
+la diferencia absoluta entre las filas es igual a la diferencia absoluta entre las columnas (pendiente).
 """
     
 
@@ -22,8 +23,11 @@ def place_queen(row, queens, n):
                 queens[row] = col
                 total_solutions += place_queen(row+1, queens, n)
         return total_solutions
+"""
+place_queen coloca una reina en cada columna posible de la fila. Si row == n; quiere decir que hemos avanzado más allá de la última
+fila (n-1): la configuración actual de queens es una solción completa 
 
-
+"""
 
 def n_queens(n):
     queens = [' ']*n
