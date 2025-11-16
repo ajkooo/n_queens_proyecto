217e261d50146cd5f16617c7578ffa073b8780e1
ajkooo <ajkoupm@gmail.com>
2025-11-16 13:01:07 +0100

    Update ppal.py

--- ppal.py
+++ ppal.py
@@ -2,10 +2,13 @@
 Programa principal
 """
 import backtracking
-import 
+import bits
 import min_conflicts
 
 N = int(input("Bienvienido al juego de las N-Reinas; introduce, a contiuacón, el tamaño del tablero"))
+
+if N < 3:
+    print("La dimensión del tablero ha de ser por lo menos d
 
 if N <= 20:
     
